ddebd9a7a3f1114b24c1803d125b4da3900197d6
Adrian <adrian.developer@yahoo.com>
2016-05-20 19:50:17 +0300

    Creating- the default project

---
+++ Microlive/Microlive/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Microlive.MvcApplication" Language="C#" %>
---
+++ Microlive/Microlive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Microlive.Startup))]
+namespace Microlive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
