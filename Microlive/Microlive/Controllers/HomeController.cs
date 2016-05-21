73e208c174afa654145d25d42f1f62ef696416a8
Adrian <adryanberca@yahoo.com>
2016-05-21 17:39:19 +0300

    Revert "Work in progress at Registration user"
    This reverts commit 0cb22dc3c78d55194162dd8e752db65f26fde1ed.

--- Microlive/Microlive/Controllers/HomeController.cs
+++ Microlive/Microlive/Controllers/HomeController.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Emgu.CV;
-using Emgu.CV.CvEnum;
-using Emgu.CV.Structure;
 
 namespace Microlive.Controllers
 {
@@ -15,6 +12,7 @@ namespace Microlive.Controllers
         {
             return View();
         }
+
 
         public ActionResult About()
         {
@@ -28,33 +26,6 @@ namespace Microlive.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-
-        public object PerformCircleCount()
-        {
-            try
-            {
-                Image<Bgr, Byte> img = new Image<Bgr, byte>(@"D:\HckTM2016\Microlive second project\examples\ShapeDetection\Images\_bloodH_F.png")
-                    .Resize(400, 400, Inter.Linear, true);
-
-                UMat uimage = new UMat();
-                CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
-
-                UMat pyrDown = new UMat();
-                CvInvoke.PyrDown(uimage, pyrDown);
-                CvInvoke.PyrUp(pyrDown, uimage);
-
-                double cannyThreshold = 5.0;
-                double circleAccumulatorThreshold = 39;
-                CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 1.6, 5, cannyThreshold, circleAccumulatorThreshold, 9, 15);
-
-                return (object)circles.Length;
-            }
-            catch (Exception ex)
-            {
-                return (object)ex.InnerException;
-            }
         }
     }
 }
--- Microlive/Microlive/Models/AccountViewModels.cs
+++ Microlive/Microlive/Models/AccountViewModels.cs
@@ -64,17 +64,6 @@ namespace Microlive.Models
 
     public class RegisterViewModel
     {
-
-
-        [Required]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-
-
-        [Required]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
