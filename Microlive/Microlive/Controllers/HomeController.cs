0cb22dc3c78d55194162dd8e752db65f26fde1ed
Adrian <adryanberca@yahoo.com>
2016-05-21 13:21:56 +0300

    Work in progress at Registration user

--- Microlive/Microlive/Controllers/HomeController.cs
+++ Microlive/Microlive/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace Microlive.Controllers
 {
@@ -12,7 +15,6 @@ namespace Microlive.Controllers
         {
             return View();
         }
-
 
         public ActionResult About()
         {
@@ -26,6 +28,33 @@ namespace Microlive.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public object PerformCircleCount()
+        {
+            try
+            {
+                Image<Bgr, Byte> img = new Image<Bgr, byte>(@"D:\HckTM2016\Microlive second project\examples\ShapeDetection\Images\_bloodH_F.png")
+                    .Resize(400, 400, Inter.Linear, true);
+
+                UMat uimage = new UMat();
+                CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
+
+                UMat pyrDown = new UMat();
+                CvInvoke.PyrDown(uimage, pyrDown);
+                CvInvoke.PyrUp(pyrDown, uimage);
+
+                double cannyThreshold = 5.0;
+                double circleAccumulatorThreshold = 39;
+                CircleF[] circles = CvInvoke.HoughCircles(uimage, HoughType.Gradient, 1.6, 5, cannyThreshold, circleAccumulatorThreshold, 9, 15);
+
+                return (object)circles.Length;
+            }
+            catch (Exception ex)
+            {
+                return (object)ex.InnerException;
+            }
         }
     }
 }
